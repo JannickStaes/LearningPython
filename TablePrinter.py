6180dd8f8cc65038f4cf810f236eec75ce3ddfca
Jannick Staes <staesjannick@gmail.com>
2021-12-18 00:13:56 +0100

    ch6

--- TablePrinter.py
+++ TablePrinter.py
@@ -1,11 +1,29 @@
 #! python3
 # prints a table of a list of string lists
 
-tableData = [['apples', 'oranges', 'cherries', 'banana'],
+tableDataExample = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-for stringList in tableData:
-    for word in stringList:
-        print(word + ' \n')
+def determineMaxColumnWidths(tableData):
+    columnWidths =  [] #a list to store the max length of each string row, to be used as the max column width
+    for stringList in tableData:
+        maxLength = 0
+        for word in stringList:
+            if len(word) > maxLength:
+                maxLength = len(word)
+        columnWidths.append(maxLength)
+    return columnWidths
 
+def printTable(tableData):
+    columnWidths = determineMaxColumnWidths(tableDataExample)
+    rows = len(tableData[0]) #all lists are equal length so we can just take the first one
+    columns = len(tableData)
+    for row in range(rows):
+        printRow = []
+        for column in range(columns):
+            printRow.append( tableData[column][row].rjust(columnWidths[column]))
+        line = ' '.join( printRow )
+        print(line)
+ 
+printTable(tableDataExample)
